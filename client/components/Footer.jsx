9094176c8c3522886b6229c0bd9a1324c761062c
Ryan Hunt <r-g-hunt@hotmail.com>
2023-11-14 13:53:14 +1300

    updated css

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <>
             <div className='footer-container'>
-                <p><a href='#pokemon-header'>&#11014;</a></p>
+                <p className='return-arrow'><a href='#pokemon-header'>&#11014;</a></p>
                 <p>Created with <a href='https://pokeapi.co/' target="_blank" rel="noreferrer">PokiAPI</a></p>
                 <p>Created by <a href='https://ryan-hunt-fwd-portfolio.web.app/' target="_blank" rel="noreferrer">Ryan Hunt</a></p>
 
