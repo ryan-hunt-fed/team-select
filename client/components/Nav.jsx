394561719b9c2abf6e559f8c6d05730e45f72b64
Ryan Hunt <r-g-hunt@hotmail.com>
2023-11-14 13:49:41 +1300

    updated css

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 function Nav() {
     return (
         <>
-            <div className='nav-container'>
+            <div className='nav-container' id='pokemon-header'>
                 {/* <div className='nav-img'>
                     <img src='./images/pokemonlogo.png' alt='pokemon-logo' id='pokemon-header' />
                 </div> */}
                 <br />
-                <div className='nav-links' id='pokemon-header'>
+                <div className='nav-links' >
                     <p><a href='#poke-search'>Search</a></p>
                     <p><a href='#poke-types'>Types</a></p>
                     <p><a href='#poke-team'>Random Team</a></p>
