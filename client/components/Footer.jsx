a48359f433b31e3da564ba5591a19194957bb092
Ryan Hunt <r-g-hunt@hotmail.com>
2023-11-19 22:22:17 +1300

    fixed some abilities with display

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -7,6 +7,9 @@ function Footer() {
                 <p className='return-arrow'><a href='#pokemon-header'>&#11014;</a></p>
                 <p>Created with <a href='https://pokeapi.co/' target="_blank" rel="noreferrer">PokiAPI</a></p>
                 <p>Created by <a href='https://ryan-hunt-fwd-portfolio.web.app/' target="_blank" rel="noreferrer">Ryan Hunt</a></p>
+                <div className='nav-img'>
+                    <img src='./images/pokemonlogo.png' alt='pokemon-logo' id='pokemon-header' />
+                </div>
 
             </div>
         </>
--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -4,13 +4,14 @@ function Nav() {
     return (
         <>
             <div className='nav-container' id='pokemon-header'>
-                {/* <div className='nav-img'>
-                    <img src='./images/pokemonlogo.png' alt='pokemon-logo' id='pokemon-header' />
-                </div> */}
-                <br />
+
+
                 <div className='nav-links' >
                     <p><a href='#poke-search'>SEARCH</a></p>
                     <p><a href='#poke-types'>TYPES</a></p>
+                    {/* <div > */}
+                    {/* <img src='./images/pokemonlogo.png' alt='pokemon-logo' id='pokemon-header' className='nav-img' /> */}
+                    {/* </div> */}
                     <p><a href='#poke-ability'>ABILITIES</a></p>
                     <p><a href='#poke-team'>RANDOM TEAM</a></p>
                 </div>
